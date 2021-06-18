e54614a0181c6e1e50402e5e2f4330424f213277
Jonathan de Castro <demoro_@live.com>
2021-06-18 15:30:26 -0300

    App incompleto de entregas, falta conseguir fazer as contas automaticamente na sessao de preco, e tambem falta conseguir fazer o banco de dados e salvar por cliente.

---
+++ RecycleView/app/src/main/java/com/devtechdmr/recycleview/Activity/Adapter/AdapterPaises.java
@@ -0,0 +1,40 @@
+package com.devtechdmr.recycleview.Activity.Adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
+
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView Local;
+        TextView Pais;
+        TextView Estado;
+
+        public MyViewHolder(View itemView){
+            super(itemView);
+        }
+
+    }
+
+}
